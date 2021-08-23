f76c7a36d4c1c2be34203ecd25f647766bd28a18
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-08-23 20:45:05 +0100

    attempt at casting and using wider dense reset

--- PrimeRust/solution_1/src/unrolled8.rs
+++ PrimeRust/solution_1/src/unrolled8.rs
@@ -1,26 +1,24 @@
-use crate::{
-    flag_storage::FlagStorage,
-    patterns::{index_pattern, MASK_PATTERNS_U8},
-};
+use std::intrinsics::transmute;
+
+use crate::{flag_storage::FlagStorage, patterns::{index_pattern, MASK_PATTERNS_U8}, unrolled32::ResetterDenseU32};
 
 pub struct FlagStorageUnrolledBits8 {
-    words: Vec<u8>,
+    words: Vec<u32>,
     length_bits: usize,
 }
 
 impl FlagStorageUnrolledBits8 {
-    const BITS: usize = u8::BITS as usize;
-
     // TODO: consider inlining
     #[inline(never)]
     fn reset_flags_sparse<const EQUIVALENT_SKIP: usize>(&mut self, skip: usize) {
-        let mask_set_index = ((EQUIVALENT_SKIP / 2) - 1) % Self::BITS;
+        // cast our u32 vector to bytes
+        let bytes: &mut[u8] = unsafe { transmute(&mut self.words[..]) };
+        let mask_set_index = ((EQUIVALENT_SKIP / 2) - 1) % 8;
         let mask_set = MASK_PATTERNS_U8[mask_set_index];
-
         let rel_indices = index_pattern::<8>(skip);
 
-        self.words.chunks_exact_mut(skip).for_each(|chunk| {
-            for i in 0..Self::BITS {
+        bytes.chunks_exact_mut(skip).for_each(|chunk| {
+            for i in 0..8 {
                 let word_idx = rel_indices[i];
                 // TODO: safety note
                 unsafe {
@@ -29,8 +27,8 @@ impl FlagStorageUnrolledBits8 {
             }
         });
 
-        let remainder = self.words.chunks_exact_mut(skip).into_remainder();
-        for i in 0..Self::BITS {
+        let remainder = bytes.chunks_exact_mut(skip).into_remainder();
+        for i in 0..8 {
             let word_idx = rel_indices[i];
             if word_idx < remainder.len() {
                 // TODO: safety note
@@ -44,50 +42,9 @@ impl FlagStorageUnrolledBits8 {
 
         // restore original factor bit -- we have clobbered it, and it is the prime
         let factor_index = skip / 2;
-        let factor_word = factor_index / Self::BITS;
-        let factor_bit = factor_index % Self::BITS;
-        if let Some(w) = self.words.get_mut(factor_word) {
-            *w &= !(1 << factor_bit);
-        }
-    }
-
-    // rather do a function call; we'll have more registers available, and it's
-    // not called very often
-    #[inline(never)]
-    fn reset_flags_dense<const SKIP: usize>(&mut self) {
-        let mask_set_index = ((SKIP / 2) - 1) % Self::BITS;
-        let mask_set = MASK_PATTERNS_U8[mask_set_index];
-
-        let rel_indices = index_pattern::<8>(SKIP);
-
-        self.words.chunks_exact_mut(SKIP).for_each(|chunk| {
-            for i in 0..Self::BITS {
-                let word_idx = rel_indices[i];
-                // TODO: safety note
-                unsafe {
-                    *chunk.get_unchecked_mut(word_idx) |= mask_set[i];
-                }
-            }
-        });
-
-        let remainder = self.words.chunks_exact_mut(SKIP).into_remainder();
-        for i in 0..Self::BITS {
-            let word_idx = rel_indices[i];
-            if word_idx < remainder.len() {
-                // TODO: safety note
-                unsafe {
-                    *remainder.get_unchecked_mut(word_idx) |= mask_set[i];
-                }
-            } else {
-                break;
-            }
-        }
-
-        // restore original factor bit -- we have clobbered it, and it is the prime
-        let factor_index = SKIP / 2;
-        let factor_word = factor_index / Self::BITS;
-        let factor_bit = factor_index % Self::BITS;
-        if let Some(w) = self.words.get_mut(factor_word) {
+        let factor_word = factor_index / 8;
+        let factor_bit = factor_index % 8;
+        if let Some(w) = bytes.get_mut(factor_word) {
             *w &= !(1 << factor_bit);
         }
     }
@@ -95,7 +52,7 @@ impl FlagStorageUnrolledBits8 {
 
 impl FlagStorage for FlagStorageUnrolledBits8 {
     fn create_true(size: usize) -> Self {
-        let num_words = size / Self::BITS + (size % Self::BITS).min(1);
+        let num_words = size / 32 + (size % 32).min(1);
         Self {
             words: vec![0; num_words],
             length_bits: size,
@@ -107,16 +64,39 @@ impl FlagStorage for FlagStorageUnrolledBits8 {
         // call into dispatcher
         // TODO: autogenerate match_reset_dispatch!(self, skip, 19, reset_flags_dense, reset_flags_sparse);
         match skip {
-            3 => self.reset_flags_dense::<3>(),
-            5 => self.reset_flags_dense::<5>(),
-            7 => self.reset_flags_dense::<7>(),
-            9 => self.reset_flags_dense::<9>(),
-            11 => self.reset_flags_dense::<11>(),
-            13 => self.reset_flags_dense::<13>(),
-            15 => self.reset_flags_dense::<15>(),
-            17 => self.reset_flags_dense::<17>(),
-            19 => self.reset_flags_dense::<19>(),
-            skip_sparse => match ((skip_sparse / 2) - 1) % Self::BITS {
+            // 3 => ResetterDenseU32::<3>::reset_dense(&mut self.words),
+            // 5 => ResetterDenseU32::<5>::reset_dense(&mut self.words),
+            // 7 => ResetterDenseU32::<7>::reset_dense(&mut self.words),
+            // 9 => ResetterDenseU32::<9>::reset_dense(&mut self.words),
+            // 11 => ResetterDenseU32::<11>::reset_dense(&mut self.words),
+            // 13 => ResetterDenseU32::<13>::reset_dense(&mut self.words),
+            // 15 => ResetterDenseU32::<15>::reset_dense(&mut self.words),
+            // 17 => ResetterDenseU32::<17>::reset_dense(&mut self.words),
+            // 19 => ResetterDenseU32::<19>::reset_dense(&mut self.words),
+            // 21 => ResetterDenseU32::<21>::reset_dense(&mut self.words),
+            // 23 => ResetterDenseU32::<23>::reset_dense(&mut self.words),
+            // 25 => ResetterDenseU32::<25>::reset_dense(&mut self.words),
+            // 27 => ResetterDenseU32::<27>::reset_dense(&mut self.words),
+            // 29 => ResetterDenseU32::<29>::reset_dense(&mut self.words),
+            // 31 => ResetterDenseU32::<31>::reset_dense(&mut self.words),
+            // 33 => ResetterDenseU32::<33>::reset_dense(&mut self.words),
+            // 35 => ResetterDenseU32::<35>::reset_dense(&mut self.words),
+            // 37 => ResetterDenseU32::<37>::reset_dense(&mut self.words),
+            // 39 => ResetterDenseU32::<39>::reset_dense(&mut self.words),
+            // 41 => ResetterDenseU32::<41>::reset_dense(&mut self.words),
+            // 43 => ResetterDenseU32::<43>::reset_dense(&mut self.words),
+            // 45 => ResetterDenseU32::<45>::reset_dense(&mut self.words),
+            // 47 => ResetterDenseU32::<47>::reset_dense(&mut self.words),
+            // 49 => ResetterDenseU32::<49>::reset_dense(&mut self.words),
+            // 51 => ResetterDenseU32::<51>::reset_dense(&mut self.words),
+            // 53 => ResetterDenseU32::<53>::reset_dense(&mut self.words),
+            // 55 => ResetterDenseU32::<55>::reset_dense(&mut self.words),
+            // 57 => ResetterDenseU32::<57>::reset_dense(&mut self.words),
+            // 59 => ResetterDenseU32::<59>::reset_dense(&mut self.words),
+            // 61 => ResetterDenseU32::<61>::reset_dense(&mut self.words),
+            // 63 => ResetterDenseU32::<63>::reset_dense(&mut self.words),
+            // 65 => ResetterDenseU32::<65>::reset_dense(&mut self.words),
+            skip_sparse => match ((skip_sparse / 2) - 1) % 8 {
                 // TODO: this needs a clean up; we're doing unnecessary conversions
                 0 => self.reset_flags_sparse::<3>(skip),
                 1 => self.reset_flags_sparse::<5>(skip),
@@ -136,7 +116,7 @@ impl FlagStorage for FlagStorageUnrolledBits8 {
         if index >= self.length_bits {
             return false;
         }
-        let word = self.words.get(index / Self::BITS).unwrap();
-        *word & (1 << (index % Self::BITS)) == 0
+        let word = self.words.get(index / 32).unwrap();
+        *word & (1 << (index % 32)) == 0
     }
 }
