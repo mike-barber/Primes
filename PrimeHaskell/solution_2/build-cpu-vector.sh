6afd1cbfa1a7bc9a85026d3ad28e82b7c44f7a14
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-09-22 08:14:44 +0100

    build process

--- PrimeHaskell/solution_2/build-cpu-vector.sh
+++ PrimeHaskell/solution_2/build-cpu-vector.sh
@@ -4,15 +4,16 @@
 #   llc --march=x86-64 --mcpu=help
 
 ARCH="x86-64"
+TRIPLE="x86_64-pc-linux-gnu"
 CPU="$1"
 echo "CPU is $CPU"
 
 rm -rf target
 mkdir -p target
-ghc -o target/Primes -outputdir target \
+ghc -v -o target/Primes -outputdir target \
     -O2 -fllvm \
     -pgmlo /usr/lib/llvm-12/bin/opt \
     -pgmlc /usr/lib/llvm-12/bin/llc \
-    -optlc -O3 -optlc --march=$ARCH -optlc --mcpu=$CPU -optlc --vectorize-slp \
-    -optlo -O3 -optlo --march=$ARCH -optlo --mcpu=$CPU -optlo --slp-vectorizer -optlo --vector-combine \
-    Primes.hs
+    -optlc -O3 -optlc -mtriple=$TRIPLE -optlc -march=$ARCH -optlc -mcpu=$CPU -optlc -mattr=+avx,+avx2 -optlc --vectorize-slp \
+    -optlo -O3 -optlo -mtriple=$TRIPLE -optlo -march=$ARCH -optlo -mcpu=$CPU -optlo -mattr=+avx,+avx2 -optlo --slp-vectorizer -optlo --vector-combine \
+    Primes.hs 2>&1
--- PrimeHaskell/solution_2/build-cpu-vector2.sh
+++ PrimeHaskell/solution_2/build-cpu-vector2.sh
@@ -4,17 +4,25 @@
 #   llc --march=x86-64 --mcpu=help
 
 ARCH="x86-64"
+TRIPLE="x86_64-pc-linux-gnu"
 CPU="$1"
 echo "CPU is $CPU"
 
-rm -rf target
-mkdir -p target
-ghc -o target/Primes -outputdir target \
+# what are these?
+# GHC Platform-specific options
+#              -mbmi2 -msse2 -msse4.2
+
+rm -rf target tmp
+mkdir -p target tmp
+
+
+ghc -v -o target/Primes -outputdir target \
+    -keep-tmp-files -tmpdir ./tmp \
     -O2 -fllvm \
     -pgmlo /usr/lib/llvm-12/bin/opt \
     -pgmlc /usr/lib/llvm-12/bin/llc \
-    -optlc -O3 -optlc --march=$ARCH -optlc --mcpu=$CPU -optlc --vectorize-slp \
-    -optlo -O3 -optlo --march=$ARCH -optlo --mcpu=$CPU \
+    -optlc -O3 -optlc -mtriple=$TRIPLE -optlc -march=$ARCH -optlc -mcpu=$CPU -optlc -mattr=+avx,+avx2 -optlc --vectorize-slp \
+    -optlo -O3 -optlo -mtriple=$TRIPLE -optlo -march=$ARCH -optlo -mcpu=$CPU -optlo -mattr=+avx,+avx2 \
     -optlo --tti \
     -optlo --targetlibinfo \
     -optlo --tbaa \
