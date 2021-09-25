271f535e1925cef547850d13a77536239c752f82
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-09-25 11:11:07 +0100

    attempting to use ghc-9 - works but complains llvm9 is supported, not llvm12

---
+++ PrimeHaskell/solution_2/build-cpu-12-stack.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# get cpu list with 
+#   llc --march=x86-64 --mcpu=help
+
+ARCH="x86-64"
+CPU="$1"
+echo "CPU is $CPU"
+
+rm -rf target tmp
+mkdir -p target tmp
+stack ghc -- -v -o target/Primes -outputdir target \
+    -keep-tmp-files -tmpdir ./tmp \
+    -O2 -fllvm -v \
+    -fsimplifier-phases=10 \
+    -mavx -mavx2 \
+    -pgmlo /usr/lib/llvm-12/bin/opt \
+    -pgmlc /usr/lib/llvm-12/bin/llc \
+    -optlc -O3 -optlc --march=$ARCH -optlc --mcpu=$CPU \
+    -optlo -O3 -optlo --march=$ARCH -optlo --mcpu=$CPU \
+    Primes.hs
